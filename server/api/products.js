88c6276b49d89be63c3bcbcc6e8aacebaee7f748
Greg Shivcharran <greg.shivcharran@gmail.com>
2021-03-02 17:59:24 -0500

    chore: changed singleproduct routes to products route and added all products route

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,16 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 
 // <-- assumes main route to products set up with app.use in index.js -->  //
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll()
+    res.send(allProducts)
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.get('/:productId', async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.productId)
