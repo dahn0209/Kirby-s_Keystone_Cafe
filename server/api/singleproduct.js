8914207ff88fd19eba77111f1ac12c32f7b5f17a
Greg Shivcharran <greg.shivcharran@gmail.com>
2021-03-02 17:21:43 -0500

    chore: added single product api route, redux, and component

---
+++ server/api/singleproduct.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+
+// <-- assumes main route to products set up with app.use in index.js -->  //
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
