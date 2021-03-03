9e02e9b925c0c1899bd9f4f4d356ac1c78a3ffb7
jlfliao <68058680+jlfliao@users.noreply.github.com>
2021-03-03 10:26:48 -0600

    Update index.js

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,9 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
-import singleProduct from './singleProduct'
+import singleProductReducer from './singleProduct'
 
-const reducer = combineReducers({user, singleProduct})
+const reducer = combineReducers({user, singleProductReducer})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
